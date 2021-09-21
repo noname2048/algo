8d1fc5f42fe60b6e0fdbbf4ad2e2a74c646e1ba2
noname2048.mac <sungwook.csw@gmail.com>
2021-09-21 20:50:22 +0900

    feat: add baekjoon-1002 wrong

---
+++ src/baekjoon-1002/wrong.py
@@ -0,0 +1,30 @@
+from math import sqrt
+
+def d(x1, y1, x2, y2):
+    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    
+def solution(x1, y1, r1, x2, y2, r2):
+    dist = d(x1, y1, x2, y2)
+    r = r1 + r2
+
+    print(dist, r)
+    
+    if dist > r:
+        return 0
+    elif dist == r:
+        return 1
+    elif dist < r:
+        if dist + r1 < r2:
+            return 0
+        elif dist + r1 == r2:
+            return 1
+        elif dist + r1 > r2:
+            return 2
+    
+    return -1
+    
+if __name__ == "__main__":
+    for testcase in range(int(input())):
+        q = list(map(int, input().split()))
+        
+        print(solution(*q))
