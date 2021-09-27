93259fcd1fda8705078a2e90cb2565506ddc0220
noname2048 <sungwook.csw@gmail.com>
2021-09-27 22:54:55 +0900

    feat: add class dfs function

--- src/baekjoon-1012/timeout.py
+++ src/baekjoon-1012/timeout.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-# import sys
+import sys
 
 dir_yx_pos = {
     "up": [1, 0],
@@ -40,7 +40,46 @@ def label(board, target_idx=1):
     return label_count
 
 
-# sys.stdin = open("input2.txt", "r")
+from copy import deepcopy
+
+
+class DfsLabel:
+    dir_x = [0, 1, 0, -1]
+    dir_y = [1, 0, -1, 0]
+
+    def __init__(self, board, target_idx=1):
+        self.row = len(board)
+        self.col = len(board[0])
+        self.board = board
+        self.lable_board = [[0] * self.row for _ in range(self.col)]
+        self.label_cnt = 0
+
+    def dfs(self, y, x):
+        self.lable_board[y][x] = self.label_cnt
+
+        for idx in range(4):
+            dy = y + self.dir_y[idx]
+            dx = x + self.dir_x[idx]
+
+            if (
+                0 <= dy < self.row
+                and 0 <= dx < self.col
+                and self.board[dy][dx] == 1
+                and self.lable_board[dy][dx] == 0
+            ):
+                self.dfs(dy, dx)
+
+    def calc(self):
+        for i in range(self.row):
+            for j in range(self.col):
+                if self.board[i][j] == 1 and self.lable_board[i][j] == 0:
+                    self.label_cnt += 1
+                    self.dfs(y, x)
+
+        return self.label_cnt
+
+
+sys.stdin = open("input2.txt", "r")
 
 for testcase in range(int(input())):
     m, n, k = list(map(int, input().split()))
@@ -54,3 +93,6 @@ for testcase in range(int(input())):
         board[y][x] = 1
 
     print(label(board))
+
+    dfs_handler = DfsLabel(board, 1)
+    print(dfs_handler.calc())
