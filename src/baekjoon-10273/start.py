e8491c8db67092c1cd6d0ce6cfb4930308078ae9
noname2048 <sungwook.csw@gmail.com>
2021-10-14 23:57:04 +0900

    feat: baekjoon-10273 close to answer

--- src/baekjoon-10273/start.py
+++ src/baekjoon-10273/start.py
@@ -1,6 +1,31 @@
 import sys
 import math
+# log = open("log.txt", "w")
+sys.stdin = open("input1.txt", "r")
 input = sys.stdin.readline
+
+global_max_cost = 0
+global_cache_path = None
+
+def dfs(here, cost, path):
+    global global_max_cost
+    global global_cache_path
+
+    # log.write(f"{here}({cost}) - {path}\n")
+    if dp_cache[here] == math.inf or dp_cache[here] < cost:
+        dp_cache[here] = cost
+
+        if global_max_cost < cost:
+            global_max_cost = cost
+            global_cache_path = path
+            # log.write(f"star\n")     
+
+        for next, develope_cost in edge[here]:
+            path = path + [next]
+            dfs(next, cost + vertex[next] - develope_cost, path)
+            path.pop()
+            
+    return dp_cache[here]
 
 t = int(input())
 for _ in range(t):
@@ -8,34 +33,16 @@ for _ in range(t):
     n, e = map(int, input().split())
     # vertex
     [*vertex] = map(int, [0] + input().split())
-    # edge weight
-    edge = [0] * (e + 1)
+    # edge weight (directional)
+    edge = [[] for _ in range(e + 2)]
     for i in range(1, e + 1):
         a, b, c = map(int, input().split())
-        edge[a].append((b, c))
-        edge[b].append((a, c))
+        edge[a].append((b, c)) # 예제 1 - 테스트 3번을 보면 분명한 방향성이 있다.
 
     dp_cache = [math.inf] * (n + 1)
-    # bfs tree 로 접근
-    path = []
-    queue = [1]
-    while queue:
-        here = queue.pop()
-        for next, cost in edge[here]:
-            vertex[next] - cost
 
-def bfs(here, cost, path):
-    if dp_cache[here] == math.inf or dp_cache[here] > cost:
-        dp_cache[here] = cost
-        for next, develope_cost in edge[here]:
-            bfs(next, cost + vertex[next] - develope_cost, path)
-            
-    return dp_cache[here]
-
-
-
-
-
-
-
-     
+    global_max_cost = 0
+    global_cache_path = []
+    value = dfs(1, vertex[1], [1])
+    print(global_max_cost, len(global_cache_path))
+    print(global_cache_path[0] if len(global_cache_path) == 1 else " ".join(map(str, global_cache_path)))
