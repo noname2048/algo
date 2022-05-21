3a42d037965682c61b83c1b5f1d1246eb83e7903
noname2048 <sungwook.csw@gmail.com>
2022-05-21 20:05:38 +0900

    improve: improve logic, but not some of wrong

--- programmers/92345/q.py
+++ programmers/92345/q.py
@@ -1,6 +1,7 @@
 # 사라지는 발판
 # 2022 블라인드 카카오 문제
 # 아침스터디
+from copy import deepcopy
 from enum import Enum
 
 
@@ -54,9 +55,17 @@ def is_same(aloc, bloc):
     return False
 
 
+def debug_board_state(board, aloc, bloc):
+    human_readable_board = deepcopy(board)
+    human_readable_board[aloc[0]][aloc[1]] = "A"
+    human_readable_board[bloc[0]][bloc[1]] = "B"
+    return human_readable_board
+
+
 # execute when 'a' turn
 def recursion_a(board, aloc, bloc):
     global ROW, COL
+    debug = debug_board_state(board, aloc, bloc)
 
     if is_zero(board, aloc):  # lose_by_falling
         return -1
@@ -70,75 +79,113 @@ def recursion_a(board, aloc, bloc):
     if len(can_move) == 0:
         return -1  # lose_by_cannot_move_anymore
 
-    cache = []
+    win = {"mn": 1000, "path": -1}
+    lose = {"mx": 0, "path": -1}
     for j in can_move:
         y, x = aloc
         new_aloc = move(j, aloc)
+
         board[y][x] = 0
         temp = -recursion_b(board, new_aloc, bloc)
-        cache += [temp]
         board[y][x] = 1
 
-    win_best, lose_best = 1000, 0
-    for k in cache:
-        if k > 0:
-            win_best = min(k, win_best)
+        if temp > 0:
+            if win["mn"] > temp:
+                win["mn"] = temp + 1
+                win["path"] = j
         else:
-            win_best = min(k, lose_best)
+            if lose["mx"] > temp:
+                lose["mx"] = temp - 1
+                lose["path"] = j
 
-    if win_best != 1000:
-        return win_best + 1
-    return lose_best - 1
+    if win["mn"] != 1000:
+        result, path = win["mn"], win["path"]
+    else:
+        result, path = lose["mx"], lose["path"]
+
+    print("A", aloc, f"r{result}", f"p{path}")
+    return result
 
 
 # execute when 'b' turn
 def recursion_b(board, aloc, bloc):
     global ROW, COL
+    debug = debug_board_state(board, aloc, bloc)
 
-    if is_zero(board, aloc):  # lose_by_falling
+    if is_zero(board, bloc):  # lose_by_falling
         return -1
 
     can_move = []
     for i in range(4):
-        new_aloc = move(i, aloc)
-        if is_inboard(new_aloc) and is_one(board, new_aloc):
+        new_bloc = move(i, bloc)
+        if is_inboard(new_bloc) and is_one(board, new_bloc):
             can_move += [i]
 
     if len(can_move) == 0:
         return -1  # lose_by_cannot_move_anymore
 
-    cache = []
+    win = {"mn": 1000, "path": -1}
+    lose = {"mx": 0, "path": -1}
     for j in can_move:
-        y, x = aloc
+        y, x = bloc
         new_bloc = move(j, bloc)
+
         board[y][x] = 0
         temp = -recursion_a(board, aloc, new_bloc)
-        cache += [temp]
         board[y][x] = 1
 
-    win_best, lose_best = 1000, 0
-    for k in cache:
-        if k > 0:
-            win_best = min(k, win_best)
+        if temp > 0:
+            if win["mn"] > temp:
+                win["mn"] = temp + 1
+                win["path"] = j
         else:
-            win_best = min(k, lose_best)
+            if lose["mx"] > temp:
+                lose["mx"] = temp - 1
+                lose["path"] = j
 
-    if win_best != 1000:
-        return win_best + 1
-    return lose_best - 1
+    if win["mn"] != 1000:
+        result, path = win["mn"], win["path"]
+    else:
+        result, path = lose["mx"], lose["path"]
+
+    print("B", bloc, f"r{result}", f"p{path}")
+    return result
 
 
-if __name__ == "__main__":
-    # q1 = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    # a, b = [1, 0], [1, 2]
-    # ans = solution(q1, a, b)
+def test_1():
+    q = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    a, b = [1, 0], [1, 2]
+    ans = solution(q, a, b)
+    assert ans == 5
 
+
+def test_2():
     q = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
     a, b = [1, 0], [1, 2]
     ans = solution(q, a, b)
+    assert ans == 4
 
-    # q = [[1, 1, 1, 1, 1]]
-    # a, b = [0, 0], [0, 4]
-    # ans = solution(q, a, b)
 
-    print(ans)
+def test_3():
+    q = [[1, 1, 1, 1, 1]]
+    a, b = [0, 0], [0, 4]
+    ans = solution(q, a, b)
+    assert ans == 4
+
+
+def test_4():
+    q = [[1]]
+    a, b = [0, 0], [0, 0]
+    ans = solution(q, a, b)
+    assert ans == 0
+
+
+def test_5():
+    q = [[1, 1, 1, 0], [1, 1, 0, 1], [1, 0, 1, 1], [0, 1, 1, 1]]
+    a, b = [0, 0], [3, 3]
+    ans = solution(q, a, b)
+    assert ans == 8
+
+
+if __name__ == "__main__":
+    test_1()
