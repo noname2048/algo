a94f9ba25658f114843bd90258b9bddc7334d730
noname2048 <sungwook.csw@gmail.com>
2022-05-29 20:32:18 +0900

    solve: solve 92342

--- programmers/92342/q.py
+++ programmers/92342/q.py
@@ -1,39 +1,57 @@
 # 양궁대회
 # 2022 카카오 블라인드 채용 level2
 import functools
+from datetime import datetime
+
+f = open("logs.txt", "w+")
+t_str = datetime.now().strftime("%H:%M - logs")
+print(t_str, file=f)
+
+mx = -999
+mx_state = []
 
 
-def re(remains, index, info):
+def re(remains, index, info, state):
+    global mx, mx_state
 
     if index > 10:
-        return 0, []
+        return 0
 
     shoot, not_shoot = 0, 0
 
     # shoot
     need_to_win = info[index] + 1
     if remains >= need_to_win:
-        temp, shoot_state = re(remains - need_to_win, index + 1, info)
+        state[index] = need_to_win
+        temp = re(remains - need_to_win, index + 1, info, state)
+        state[index] = 0
         shoot = temp + (10 - index)
 
     # not shoot
-    temp, not_shoot_state = re(remains, index + 1, info)
+    temp = re(remains, index + 1, info, state)
     if info[index] == 0:
         not_shoot = temp + 0
     else:
         not_shoot = temp - (10 - index)
 
     if shoot > not_shoot:
-        state = [need_to_win] + shoot_state
-        return shoot, state
+        print(f"shoot {index:2d}, {mx_state} {mx}", file=f)
+        if mx < shoot:
+            mx = shoot
+            mx_state = state.copy()
+        return shoot
     else:
-        state = [0] + not_shoot_state
-        return not_shoot, state
+        print(f" n ot {index:2d}, {mx_state} {mx}", file=f)
+        if mx < not_shoot:
+            mx = not_shoot
+            mx_state = state.copy()
+        return not_shoot
 
 
 def solution(n, info):
-    score, state = re(n, 0, info)
-    return state
+    state = [0] * 11
+    score = re(n, 0, info, state)
+    return mx_state
 
 
 def test_1():
@@ -43,3 +61,7 @@ def test_1():
 
     answer = solution(n, info)
     assert ans == answer
+
+
+test_1()
+f.close()
--- programmers/92342/q3.py
+++ programmers/92342/q3.py
@@ -1,3 +1,6 @@
+# Too many wrong codes, so not split file by file
+# I try fix this code in here
+
 from datetime import datetime
 
 f = open("logs.txt", "w+")
@@ -9,20 +12,26 @@ class Problem:
     def __init__(self, n, info):
         self.n = n
         self.info = info
-        self.mx = -56
+        self.mx = 0
         self.mx_state = [0] * 11
 
     def re(self, remains, idx, state, score):
-        if remains == 0:
+        if idx == 10:
+            state[idx] = remains
+            print(f"{remains:2d} {idx:2d} {state} {score:3d} {self.mx:2d}", file=f)
             if self.mx < score:
                 self.mx = score
                 self.mx_state = state.copy()
-            return
+            elif self.mx == score:
+                for i in range(10, -1, -1):
+                    lo, gl = state[i], self.mx_state[i]
+                    if lo > gl:
+                        self.mx_state = state.copy()
+                        break
+                    elif lo < gl:
+                        break
 
-        if idx > 10:
-            if self.mx < score:
-                self.mx = score
-                self.mx_state = state.copy()
+            state[idx] = 0
             return
 
         # win
@@ -42,6 +51,9 @@ def solution(n, info):
     problem = Problem(n, info)
     state = [0] * 11
     problem.re(n, 0, state, 0)
+
+    if problem.mx <= 0:
+        return [-1]
     return problem.mx_state
 
 
@@ -59,5 +71,12 @@ def test_2():
     print(solution(n, info))
 
 
+def test_3():
+    n = 9
+    info = [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]
+    ans = [1, 1, 2, 0, 1, 2, 2, 0, 0, 0, 0]
+    print(solution(n, info))
+
+
 # test_1()
-test_2()
+test_3()
