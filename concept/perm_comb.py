92cb643bba0830c248abb9d6722c5efa766d607f
noname2048 <sungwook.csw@gmail.com>
2022-06-10 23:44:45 +0900

    feat: add new combi method

--- concept/perm_comb.py
+++ concept/perm_comb.py
@@ -59,3 +59,23 @@ def permutation(items, k):
 ans = permutation(items, 3)
 print(f"permutation ({len(ans)})")
 print(ans)
+
+# combination을 전부구하는 새로운 방법
+# ------------------------------------
+nums = [1, 2, 3, 4, 5]
+answer_list = []
+
+
+def combi(n, ans):
+    if n == len(nums):
+        temp = [i for i in ans]
+        answer_list.append(temp)
+        return
+    ans.append(nums[n])
+    combi(n + 1, ans)
+    ans.pop()
+    combi(n + 1, ans)
+
+
+combi(0, [])
+print(answer_list)
