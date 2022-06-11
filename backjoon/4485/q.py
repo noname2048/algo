78d9cda066b031bd04bad9be2b08665cb149d4ac
noname2048 <sungwook.csw@gmail.com>
2022-06-11 23:40:41 +0900

    timeout: 4485

--- backjoon/4485/q.py
+++ backjoon/4485/q.py
@@ -10,27 +10,73 @@ DIR = [
 ]
 
 
-def solution(cave):
-    global N
-    connection = defaultdict(dict)
-    adj = [[math.inf for _ in range(N)] for _ in range(N)]
-    for y in range(N):
-        for x in range(N):
-            pos = (y, x)
-            idx = y * N + x
-            for i in range(4):
-                ny, nx = (pos[0] + DIR[i][0], pos[1] + DIR[i][1])
-                if 0 <= ny < N and 0 <= nx < N:
-                    nxt_idx = ny * N + nx
-                    connection[idx][nxt_idx] = cave[ny][nx]
-                    connection[nxt_idx][idx] = cave[y][x]
+def solution(N, cave):
+    mx = math.inf
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    visited[0][0] = True
+
+    def dfs(pos, value):
+        global DIR
+        nonlocal cave, visited, mx
+        if pos == (N - 1, N - 1):
+            mx = min(mx, value)
+            return
+
+        if value > mx:
+            return
+
+        for i in range(4):
+            ny, nx = pos[0] + DIR[i][0], pos[1] + DIR[i][1]
+            if 0 <= ny < N and 0 <= nx < N and not visited[ny][nx]:
+                visited[ny][nx] = True
+                dfs((ny, nx), value + cave[ny][nx])
+                visited[ny][nx] = False
+
+    dfs((0, 0), cave[0][0])
+    return mx
+
+
+def predata():
+    def make_gen():
+        yield from """3
+        5 5 4
+        3 9 1
+        3 2 7
+        5
+        3 7 2 0 1
+        2 8 0 9 1
+        1 2 1 8 1
+        9 8 9 2 0
+        3 6 5 1 5
+        7
+        9 0 5 1 1 5 3
+        4 1 2 1 6 5 3
+        0 7 6 1 6 8 5
+        1 1 7 8 3 2 3
+        9 4 0 7 6 4 1
+        5 8 3 2 4 8 3
+        7 4 8 4 8 3 4
+        0""".splitlines()
+
+    gen = make_gen()
+
+    def get_iter():
+        nonlocal gen
+        return next(gen)
+
+    return get_iter
 
 
 if __name__ == "__main__":
-    C = int(input())
+    # input = predata()
     N = int(input())
-    for c in range(C):
+    while N != 0:
         cave = []
         for i in range(N):
-            temp = input().split(" ")
-            cave.append([map(int, temp)])
+            temp = input().strip().split(" ")
+            cave.append(list(map(int, temp)))
+
+        ans = solution(N, cave)
+        print(ans)
+
+        N = int(input())
