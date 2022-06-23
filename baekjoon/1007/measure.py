ee2b5ebc593dd46808a7d30a162d1bcaa7567d83
noname2048 <sungwook.csw@gmail.com>
2022-06-23 22:17:20 +0900

    try: baekjoon 1002
    - finding what is best list appending methods?
    - modifing is worst
    - learn how to timeit what globals
    - :param numbers: means repeat numbers

---
+++ baekjoon/1007/measure.py
@@ -0,0 +1,43 @@
+import timeit
+
+
+def make_pairs_a(n: int):
+    pairs = []
+    for i in range(n):
+        pair = (i * 2 + 1, i * 2 + 2)
+        pairs.append(pair)
+    return pairs
+
+
+def make_pairs_b(n: int):
+    pairs = [[0, 0] for _ in range(n)]
+    for i in range(n):
+        pairs[i][0], pairs[i][1] = i * 2 + 1, i * 2 + 2
+
+
+def make_pairs_c(n: int):
+    pairs = []
+    for i in range(n):
+        pairs += [(i * 2 + 1, i * 2 + 2)]
+
+
+def make_pairs_d(n: int):
+    pairs = []
+    for i in range(n):
+        pairs.append([i * 2 + 1, i * 2 + 2])
+
+
+a = timeit.timeit("ans = make_pairs_a(100)", globals=globals())
+b = timeit.timeit("ans = make_pairs_b(100)", globals=globals())
+c = timeit.timeit("ans = make_pairs_c(100)", globals=globals())
+d = timeit.timeit("ans = make_pairs_d(100)", globals=globals())
+print(a, b, c, d)
+
+# 결과
+# a 20.993
+# b 30.639
+# c 23.062
+# d 27.731
+
+# 이상하게도 리스트를 append 하는 것 보다,
+# 만들어두고 수정을 하는게 더 오래 걸렸다.
