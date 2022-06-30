8bf42ba13ea869bea6585fbf5924440b76b9f144
noname2048 <sungwook.csw@gmail.com>
2022-06-30 23:31:25 +0900

    feat: baekjoon(1013), add freopen func

--- baekjoon/1013/q.py
+++ baekjoon/1013/q.py
@@ -1,14 +1,36 @@
 from sys import stdin
 import re
 
+
+def freopen():
+    def make_generator():
+        s = map(
+            lambda x: x.strip(),
+            """3
+            10010111
+            011000100110001
+            0110001011001
+            """.splitlines(),
+        )
+        yield from s
+
+    generator = make_generator()
+
+    def iter():
+        return next(generator)
+
+    return iter
+
+
 input = stdin.readline
+input = freopen()
 
 
 def solve():
     pattern = re.compile(r"(100+1+|01)+")
     testcase = int(input())
-    for i in range(testcase):
-        wave = input()
+    for _ in range(testcase):
+        wave = input().strip()
         match = pattern.match(wave)
         if match and match.start() == 0 and match.end() == len(wave):
             print("YES")
