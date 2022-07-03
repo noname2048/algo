e49669f0ebe179f60a880e33ca4f15085ba95a94
noname2048 <sungwook.csw@gmail.com>
2022-07-03 20:18:22 +0900

    feat: baekjoon(1016), memory out 3

---
+++ baekjoon/1016/q3.py
@@ -0,0 +1,30 @@
+from sys import stdin
+
+input = stdin.readline
+
+
+def solve():
+    mn, mx = map(int, input().split())
+
+    numbers = set()
+
+    num = 2
+    square_num = num**2
+    while square_num <= mx:
+
+        q, r = divmod(square_num, mn)
+        mi = square_num * q if mn == 0 else square_num * (q + 1)
+
+        while mi <= mx:
+            numbers.add(mi)
+            mi += square_num
+
+        num += 1
+        square_num = num**2
+
+    ans = mx - mn + 1 - len(numbers)
+    print(ans)
+
+
+if __name__ == "__main__":
+    solve()
