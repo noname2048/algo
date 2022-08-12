c6b30aff120b78308458d8ff958d61f5c1017563
sungwook.choi <sungwook.csw@gmail.com>
2022-08-12 23:39:18 +0900

    feat: baekjoon(1019), good trial

--- baekjoon/1019/q2.py
+++ baekjoon/1019/q2.py
@@ -1,31 +1,76 @@
-def one(n):
-    ret = 0
-    # quotient, remainder
-    q, r = divmod(n, 10)
-    k += min(n, 1) - max(n, 0)
+"""
+일의 자리 수에서 1 보다 크면 1 에 10의 자리수 마다 반복
+십의 자리 수에서 10 ~ 19 까지 + 10
+백의 자리 수에서 100 ~ 199 까지  + 100
+천의 자리 수에서 1000 ~ 1999 까지  + 1000
 
-    q, r = divmod(n, 100)
-    k += q * 10
-    if r >= 10:
-        k += max(r - 10 + 1, 10)
+2를 가지고 해보자
+일의 자리 수에서 2보다 크면 1
+십의 자리 수에서 20~29 까지  + 10
+...
 
-    q, r = divmod(n, 1000)
-    k += q * 100
-    if r >= 100:
-        k += max(r - 100 + 1, 100)
+0은 좀 다를거야
+일의 자리수에서 10으로 나눈 몫 마다 + 1
+십의 자리 수에서 100 ~ 109 까지 추가로 0이 10개 추가
+백의 자리 수에서 1000 ~ 1099까지 추가로 0이 100개 추가
+"""
 
-    count = 0
-    s = 1
-    topic_str, topic = s + "0", int(s + "0")
-    q, r = divmod(n, topic)
-    while q:
-        count += q
-        if r >= topic:
-            count += max(r - topic + 1, 10**1)
+from re import I
 
 
-def solve():
-    n = int(input())
+def calc(n, target):
+    length = len(str(n))
+
+    k = 0
+    for i in range(length):
+        unit = pow(10, i)
+        q, r = divmod(n, unit * 10)
+        k += q * unit
+        if r >= (target + 1) * unit:
+            k += unit
+        elif r >= target * unit:
+            k += r - target * unit + 1
+
+    return k
+
+
+"""
+0이 좀 어렵네
+10으로 나눠서 몫이 나오면 그만큼 더하기
+100으로 나눠서 몫이 나오면 110까지 계산하기
+1000으로 나눠서 몫이 나오면 1100까지 계산하기
+
+105 
+10 20 30 ... 90 100 중 일의자리 (10개)
+100 ~ 105 까지 십의자리 6개
+"""
+
+
+def calc2(n):
+    length = len(str(n))
+
+    k = 0
+    for i in range(length):
+        unit = pow(10, i)
+        q, r = divmod(n, unit * 10)
+        k += (q - 1) * unit
+        if r >= 1 * unit:
+            k += unit
+        else:
+            k += r + 1
+
+    return k
+
+
+def solve(n=None):
+    if not n:
+        n = int(input())
+    ans = [calc2(n)]
+    for i in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        ans.append(calc(n, i))
+
+    print(" ".join(map(str, ans)))
+    return ans
 
 
 def test_solve():
@@ -65,4 +110,6 @@ def test_solve():
 
 if __name__ == "__main__":
 
-    pass
+    solve(11)
+    solve(7)
+    solve(19)
