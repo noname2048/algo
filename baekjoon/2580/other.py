9ded925adc4bc2aa437e800b73875f82a6744884
sungwook.choi <sungwook.csw@gmail.com>
2022-09-03 22:38:56 +0900

    chore: baekjoon(2580), try to read others

--- baekjoon/2580/other.py
+++ baekjoon/2580/other.py
@@ -17,3 +17,19 @@ unitlist = (
 )
 units = dict((s, [u for u in unitlist if s in u]) for s in squares)
 peers = dict((s, set(sum(units[s], [])) - set([s])) for s in squares)
+
+
+def parse_grid(board):
+    values = dict((s, digits) for s in squares)
+    for s, d in grid_values(board).items():
+        if d in digits and not assign(values, s, d):
+            return False
+    return values
+
+
+def assign(values, s, d):
+    other_values = values[s].replace(d, "")
+    if all(eliminate(values, s, d2) for d2 in other_values):
+        return values
+    else:
+        return False
