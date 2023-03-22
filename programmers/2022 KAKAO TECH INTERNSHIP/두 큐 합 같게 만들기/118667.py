7bb2c9a081e997725503d449de619b872a7c908c
Sungwook Choi <sungwook.csw@gmail.com>
2023-03-22 11:25:09 +0900

    solve: 118667

--- programmers/2022 KAKAO TECH INTERNSHIP/두 큐 합 같게 만들기/118667.py
+++ programmers/2022 KAKAO TECH INTERNSHIP/두 큐 합 같게 만들기/118667.py
@@ -1,62 +1,68 @@
-from dataclasses import dataclass
-
-@dataclass
-class Q1:
-    queue1 = [3, 2, 7, 2]
-    queue2 = [4, 6, 5, 1]
-
-def solve():
-    p = Q1
-    solution(p.queue1, p.queue2)
-
 def solution(queue1, queue2):
-    answer = -2
-    return answer
-
-
-def inner_solution(q1, q2):
-    q3 = q1 + q2
-    l1, l2, l3 = len(q1), len(q2), len(q3)
-
-    sum = sum(q3)
-    if sum % 2 != 0:
+    # 합이 짝수가 아닐경우
+    total = sum(queue1) + sum(queue2)
+    if total % 2 == 1:
         return -1
+
+    # 두개로 나누어 풀기
+    a1 = sub_solution(queue1, queue2, total)
+    a2 = sub_solution(queue2, queue1, total)
+
+    ans = -1
+    if a1 == -1 and a2 > -1:
+        ans = a2
+    elif a2 == -1 and a1 > -1:
+        ans = a1
+    else:
+        ans = min(a1, a2)
+
+    print(ans)
+    return ans
+
+
+def sub_solution(q1, q2, sumation):
+    q3 = q1.copy() + q2.copy()
+
+    half = sumation / 2
 
     ans = -1
 
-    for_debug = []
-    def update_ans(num: int) -> None:
-        for_debug.append(num)
+    def update_ans(num):
+        nonlocal ans
         if ans == -1 or num < ans:
             ans = num
 
-    # i 포함, j 미포함, s 합
-    i, j, s, half = 0, 0, 0, sum / 2
-    while i < l3:
-        if s < half and j <= l3:
+    i, j, s = 0, 0, 0
+    while i < len(q1):
+        if s < half and j < len(q3):
             s += q3[j]
             j += 1
-        elif s > half and i < l3:
+        elif s > half:
             s -= q3[i]
             i += 1
         elif s == half:
-            if j < l1:
-                update_ans(j + l2 + i)
-            if j == l1:
+            if j == len(q1):
                 update_ans(i)
-            if j < l2:
-                if i < l1:
-                    update_ans(i + j - l1)
-                elif i >= l1:
-                    update_ans((j - l1) + l1 + (i - l1))
-            if j == l2:
-                if i < l1:
-                    update_ans(i + l2)
-                elif i >= l1:
-                    update_ans(i - l1)
-            #
+            elif j < len(q1):
+                update_ans(len(q2) + j + i)
+            elif j > len(q1):
+                update_ans(j - len(q1) + i)
+            else:
+                print("Error")
             s -= q3[i]
             i += 1
         else:
             break
 
+    return ans
+
+
+Q1 = {
+    "queue1": [3, 2, 7, 2],
+    "queue2": [4, 6, 5, 1],
+}
+Q2 = {
+    "queue1": [1, 2, 1, 2],
+    "queue2": [1, 10, 1, 2],
+}
+solution(**Q2)
