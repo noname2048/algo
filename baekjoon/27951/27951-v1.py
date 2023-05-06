633aabbf8741747ea3b66bde3405e6f8cfa818f6
Sungwook Choi <sungwook.csw@gmail.com>
2023-05-06 11:07:52 +0900

    feat: solve 27925

--- baekjoon/27951/27951-v1.py
+++ baekjoon/27951/27951-v1.py
@@ -1,13 +1,32 @@
+# 3660ms 맞았습니다 (헉)
+
 import sys
 
-sys.stdin = open("p1.txt", "r")
+sys.stdin = open("p3.txt", "r")
 input = lambda: sys.stdin.readline().strip()
 
+
 def thousand2abc(n):
-    return n // 100, n // 10, n % 10
+    return n // 100, (n % 100) // 10 % 10, n % 10
+
 
 def abc2thousand(a, b, c):
     return a * 100 + b * 10 + c
+
+
+def dist(u, v):
+    return min(abs(u - v), 10 - abs(u - v))
+
+
+def get_new_status(current, number):
+    a, b, c = thousand2abc(current)
+    new_status = [
+        abc2thousand(number, b, c),
+        abc2thousand(a, number, c),
+        abc2thousand(a, b, number),
+    ]
+    return new_status
+
 
 def main():
     n = int(input())
@@ -16,34 +35,45 @@ def main():
     # 그리디로 풀 수 있는 점화식 발견 실패
     # dp 나 탐색에서 수를 줄일 수 있는 방안으로
 
-    cache = [[0] * 1000 for _ in range(n)]
+    cache = [[-1] * 1000 for _ in range(n)]
 
     k = t[0]
-    cache[0][abc2thousand(k, 0, 0)] = k
+    d = dist(0, k)
+    cache[0][abc2thousand(k, 0, 0)] = d # 무조건 첫번째 화구 사용
 
-    for n_idx in range(1000):
+    for n_idx in range(1, n):
         for status_idx in range(1000):
-            if cache[n_idx - 1][status_idx] != -1:
-                status = thousand2abc(status_idx)
-                for i in range(3):
-                    diff = (status[i] - t[n_idx] + 10) % 10
-                    status[i] = t[n_idx]
+            # 이전 결과값 없음
+            if cache[n_idx - 1][status_idx] == -1:
+                continue
 
-                    new_status = abc2thousand(*status)
-                    if cache[n_idx][new_status] == -1: # 결과값 없음
-                        cache[n_idx][new_status] = cache[n_idx][status] + diff
-                    else: # 최소 결과값 사용
-                        cache[n_idx][new_status] = min(cache[n_idx][new_status], cache[n_idx][status] + diff)
+            current_abc = thousand2abc(status_idx)
+            new_status = get_new_status(status_idx, t[n_idx])
 
-        a, b, c = thousand2abc(i)
-        cache[i][i] = abs(t[0] - abc2thousand(a, b, c))
+            for i in range(3):
+                next_status = new_status[i]
+                d = dist(current_abc[i], t[n_idx])
+                if cache[n_idx][next_status] == -1:  # 결과값 없음
+                    cache[n_idx][next_status] = cache[n_idx - 1][status_idx] + d
+                else:  # 최소 결과값 status_idx
+                    cache[n_idx][next_status] = min(
+                        cache[n_idx][next_status], cache[n_idx -1][status_idx] + d
+                    )
 
-    def re(idx):
-        if idx == n:
-            return
+    ## for debug
+    # with open("log.txt", "w") as f:
+    #     for i in range(n):
+    #         f.write(f"{i:02d}회차---\n")
+    #         for j in range(1000):
+    #             if cache[i][j] != -1:
+    #                 f.write(f"{j:03d} -- \t{cache[i][j]}\n")
 
-        for a in range(10):
-            for b in range(10):
-                for c in range(10):
-                    if cache[idx][a][b][c] != -1:
-                        t[n-1][a] = 
+    answer = 90_000
+    for i in range(1000):
+        if cache[n - 1][i] != -1:
+            answer = min(answer, cache[n - 1][i])
+
+    print(answer)
+
+
+main()
