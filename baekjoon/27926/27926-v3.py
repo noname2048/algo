88a98839332102e66820897203014f07b60e168b
Sungwook Choi <sungwook.csw@gmail.com>
2023-05-14 00:33:01 +0900

    timeout: 27926

--- baekjoon/27926/27926-v3.py
+++ baekjoon/27926/27926-v3.py
@@ -5,15 +5,19 @@ from collections import defaultdict
 sys.stdin = open("p2.txt", "r")
 input = lambda: sys.stdin.readline().rstrip()
 
+
 def main():
     edges = []
     n, m = list(map(int, input().split()))
     for _ in range(m):
         u, v, d = list(map(int, input().split()))
         edges.append((u, v, d))
-    answer = solve01(edges)
-    answer = max(answer, solve02(n, m, edges))
+
+    a = solve01(edges)
+    b = solve02(n, m, edges)
+    answer = max(a, b)
     print(answer)
+
 
 def solve01(edges):
     # case 01
@@ -30,82 +34,48 @@ def solve01(edges):
     answer = sum(poped.values())
     return answer
 
+
 def solve02(n, m, edges):
     # case 02
-    edge_dict = defaultdict(dict)
-    for idx, edge in edges:
+    adj = defaultdict(list)
+    for i, edge in enumerate(edges):
         u, v, d = edge
-        edge_dict[u] = (idx, d)
-        edge_dict[v] = (idx, d)
+        adj[u].append((i, v, d))
+        adj[v].append((i, u, d))
 
+    for node in adj.keys():
+        adj[node] = list(sorted(adj[node], key=lambda x: x[2], reverse=True))
+
+    answer = 0
     for idx, edge in enumerate(edges):
-        # 정점 u 에서 v 를 제외하고 간선이 1개 이상인가
-        a, b, d = edge
-        a_dict = {k: v for k, v in edge_dict[u].items() if k != b}
-        b_dict = {k: v for k, v in edge_dict[v].items() if k != a}
-        a_dict = sorted(a_dict)
-        b_dict = sorted(b_dict)
-
-
-        edge_candidate = set()
-        for i, d, in edge_dict[u]:
-            if i != idx:
-                edge_candidate.add((i, d))
-            
-        for j, d in edge_dict[v]:
-            if j != idx:
-                edge_candidate.add((j, d))
-
-
-        edge_candidate = set()
-        for i, d in edge_dict[u]:
-            edge_candidate.add((i, d))
-        for j, u, d in edge_dict[v]:
-            edge_candidate.add((j, d))
-        edge_candidate.remove((idx, edge[2]))
-
-        # 간선이 사이클을 이루는 경우 밖에 없는 경우 제외
-        if len(edge_candidate) < 2:
+        u, v, d = edge
+        # 정점이 각각 고려할 수 있는 간선이 있는가 확인
+        if len(adj[u]) < 2 or len(adj[v]) < 2:
             continue
 
-        # 
+        # 지금 고려하고 있는 간선을 후보군에서 제거
+        u_adj = [e for e in adj[u] if e[0] != idx]
+        v_adj = [e for e in adj[v] if e[0] != idx]
 
+        # 각 정점에서 2개 후보군을 뽑는다.
+        u_cand = [u_adj[0], (-1, -1, 0)]
+        v_cand = [v_adj[0], (-1, -1, 0)]
 
-    for node in edge_dict.keys():
-        edge_dict[node] = dict(sorted(edge_dict[node].items(), key=lambda x: x[1], reverse=True))
-    
-    for edge in edges:
-        local_answer = -1
-        u, v, d = edge
-        result_u = [(-1, 0), (-1, 0)]
-        result_v = [(-2, 0), (-2, 0)]
+        if len(u_adj) > 1:
+            u_cand[1] = u_adj[1]
+        if len(v_adj) > 1:
+            v_cand[1] = v_adj[1]
 
-        local_u = {k: v for k, v in edge_dict[u].items() if k != v}   
-        local_v = {k: v for k, v in edge_dict[v].items() if k != u}
-
-        u_keys = list(local_u.keys())
-        u_values = list(local_u.values())
-        v_keys = list(local_v.keys())
-        v_values = list(local_v.values())
-
-        if len(u_keys) > 0:
-            result_u[0] = (u_keys[0], u_values[0])
-        if len(u_keys) > 1:
-            result_u[1] = (u_keys[1], u_values[1])
-
-        if len(v_keys) > 0:
-            result_v[0] = (v_keys[0], v_values[0])
-        if len(v_keys) > 1:
-            result_v[1] = (v_keys[1], v_values[1])
-        
-        if result_u[0][0] == result_v[0][0]:
-            a = result_u[0][1] + result_v[1][1]
-            b = result_v[0][1] + result_u[1][1]
-            local_answer = max(a, b)
+        # 각 정점의 가장 큰 후보의 타겟이 같지 않다면 활용
+        if u_cand[0][1] != v_cand[0][1]:
+            local_answer = u_cand[0][2] + v_cand[0][2] + d
         else:
-            local_answer = result_u[0][1] + result_v[0][1]
-        answer = max(answer, local_answer + d)
+            a = u_cand[0][2] + v_cand[1][2] + d
+            b = u_cand[1][2] + v_cand[0][2] + d
+            local_answer = max(a, b)
 
+        answer = max(answer, local_answer)
     return answer
+
 
 main()
